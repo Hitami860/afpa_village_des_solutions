fcd1452c34ef0fb29a0da29d9cd0586c2c9dc436
Hitami860 <Hitami86000@gmail.com>
2024-12-02 16:06:32 +0100

    upd

--- src/Controller/Admin/UsersCrudController.php
+++ src/Controller/Admin/UsersCrudController.php
@@ -3,28 +3,38 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Users;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Encoder\EncoderFactory;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 class UsersCrudController extends AbstractCrudController
 {
+
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Users::class;
     }
 
-    
     public function configureFields(string $pageName): iterable
     {
         return [
             TextField::new('accountNumber'),
             EmailField::new('email'),
-            TextField::new("password"),
+            TextField::new("password")->hideWhenUpdating(),
             ChoiceField::new("roles")->setChoices([
                 'AFPA' => 'ROLE_AFPA',
                 'PARTNER' => 'ROLE_PARTENAIRE',
@@ -33,5 +43,31 @@ class UsersCrudController extends AbstractCrudController
             AssociationField::new('partner'),
         ];
     }
-    
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->hashPassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->hashPassword($entityInstance);
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function hashPassword($user)
+    {
+        if (!$user instanceof Users) {
+            return;
+        }
+
+        if ($user->getPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+        }
+    }
 }
