b94b1fc5a501c5ec3d8f04ed9e29bd667a198097
Hitami860 <Hitami86000@gmail.com>
2024-12-03 16:30:20 +0100

    upd

--- src/Controller/InterventionsController.php
+++ src/Controller/InterventionsController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Interventions;
+use App\Entity\Partner;
+use App\Form\InterventionsType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class InterventionsController extends AbstractController
 {
+
+    public function __construct(
+        private readonly EntityManagerInterface $entityManagerInterface,
+    )
+    {}
+
     #[Route('/interventions', name: 'app_interventions')]
-    public function index(): Response
+    public function planInterevntions(Request $request): Response
     {
-        return $this->render('interventions/index.html.twig', [
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $intervention = new Interventions();
+        $form = $this->createForm(InterventionsType::class, $intervention);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $intervention->setPartner($this->getUser()->getPartner());
+            $this->entityManagerInterface->persist($intervention);
+            $this->entityManagerInterface->flush();
+
+        }
+
+        return $this->render('interventions/interventions.html.twig', [
             'controller_name' => 'InterventionsController',
+            'form' => $form->createView()
         ]);
     }
 }
