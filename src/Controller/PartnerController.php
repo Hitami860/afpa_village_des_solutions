958df1098d1a8d284023bab6a163b189252e4c43
Hitami860 <Hitami86000@gmail.com>
2024-12-12 15:32:29 +0100

    upd

--- src/Controller/PartnerController.php
+++ src/Controller/PartnerController.php
@@ -8,6 +8,7 @@ use App\Entity\Partner;
 use App\Form\PartnerType;
 use App\Repository\ActivitiesRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,22 +27,30 @@ class PartnerController extends AbstractController
     }
 
     #[Route('/partner/{id}', name: 'app_partner')]
-    public function partnerid($id, EntityManagerInterface $entity): Response
+    public function partnerid($id, EntityManagerInterface $entity, Request $request, PaginatorInterface $paginatorInterface): Response
     {
-        
+
         // $partner = $entity->getRepository(Partner::class)->findAll();
         $partner = $entity->getRepository(Partner::class)->findOneBy(['id' => $id]);
         $interventions = $entity->getRepository(Interventions::class)->findBy(['partner' => $partner]);
         $activities = $entity->getRepository(Activities::class)->findBy(['partner' => $partner]);
+
+        $partner = $this->getUser()->getPartner();
+        $interventions = $partner->getInterventions();
+
+        $interv = $paginatorInterface->paginate(
+            $interventions,
+            $request->query->getInt('page', 1),
+            4
+        );
+
         return $this->render('partner/partner.html.twig', [
             'controller_name' => 'PartnerController',
             'id' => $id,
             'partner' => $partner,
-            // 'partner'=> $partner,
-            'interventions'=> $interventions,
-            'activities'=>$activities
+            'interventions' => $interventions,
+            'activities' => $activities,
+            'interv' => $interv,
         ]);
     }
-
-    
 }
